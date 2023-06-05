86d83b88e1b6b6f596d88c11b8c0e44a0128aabc
Ndongo <52391170+ndongo78@users.noreply.github.com>
2023-06-05 22:31:46 +0200

    Update dbConfig.py

--- api/db/dbConfig.py
+++ api/db/dbConfig.py
@@ -6,6 +6,6 @@ import dns.resolver
 dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
 dns.resolver.default_resolver.nameservers = ['8.8.8.8']
 
-client = MongoClient("mongodb+srv://ndongo:a9125844@cluster0.583znhe.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient("")
 db = client.Cluster0
 collection = db.get_collection("users")
